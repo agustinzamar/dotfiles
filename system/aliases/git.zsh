17ec329028778771f8dbf144cd5a27d76a11ec0f
Agustin Zamar <agustinzamar33@gmail.com>
2026-07-29 15:51:25 -0300

    feat: add lazygit as git TUI
    - brew lazygit in install/topics/dev
    - config/lazygit/config.yml wired to delta pager + nvim editor
    - symlink to ~/.config/lazygit/config.yml in install/links.sh
    - lg alias in system/aliases/git.zsh

--- system/aliases/git.zsh
+++ system/aliases/git.zsh
@@ -5,3 +5,4 @@ alias gpo="git push origin"
 alias uncommit="git reset --soft HEAD~1"
 alias branches="git branch --sort=committerdate | head -10"
 alias gecor='git checkout $(git branch -a | fzf)'
+alias lg='lazygit'
