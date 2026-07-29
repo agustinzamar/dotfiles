1c41f4947cac28ac4d48640e3342e826a42ea200
Agustin Zamar <agustinzamar33@gmail.com>
2026-07-28 23:12:28 -0300

    fix: update opencode alias to point to opencode2 in editors.zsh

--- system/aliases/editors.zsh
+++ system/aliases/editors.zsh
@@ -1,5 +1,4 @@
 alias phpstorm='open -a "PhpStorm.app" "$(pwd)"'
 alias cl='command claude'
-alias oc='command opencode'
+alias oc='command opencode2'
 alias cx='command codex'
-alias opencode='command opencode2'
