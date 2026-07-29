af546636d11e4abad161ac62fa665f10aa672a09
Agustin Zamar <agustinzamar33@gmail.com>
2026-07-29 13:42:10 -0300

    feat: enable zmv for batch renaming and add related aliases

--- system/aliases/filesystem.zsh
+++ system/aliases/filesystem.zsh
@@ -4,4 +4,8 @@ alias ls='eza -la --icons --group-directories-first'
 alias ll='eza -la --icons --git'
 alias lt='eza --tree --icons'
 
+# zmv: batch rename/copy/link without the shell expanding glob patterns
+alias zmv='noglob zmv'
+alias zcp='noglob zmv -C'
+alias zln='noglob zmv -L'
 
