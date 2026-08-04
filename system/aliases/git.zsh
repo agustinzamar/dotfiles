75f82bf17c836a7f0d114447e5b47ec8d3875e42
Agustin Zamar <agustinzamar33@gmail.com>
2026-08-04 16:45:09 -0300

    Remove git aliases duplicated by oh-my-zsh git plugin

--- system/aliases/git.zsh
+++ system/aliases/git.zsh
@@ -1,6 +1,3 @@
-alias nah="git reset --hard;git clean -df"
-alias push="git push"
-alias pull="git pull"
 alias gpo="git push origin"
 alias uncommit="git reset --soft HEAD~1"
 alias branches="git branch --sort=committerdate | head -10"
