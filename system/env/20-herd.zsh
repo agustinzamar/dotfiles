de2537c3f424e8bfb6a831c38e13e55a41c5e9d3
Agustin Zamar <agustinzamar33@gmail.com>
2026-08-15 11:27:41 -0300

    remove obsolete node wrapper path hook

--- system/env/20-herd.zsh
+++ system/env/20-herd.zsh
@@ -14,19 +14,6 @@ export NVM_DIR="$HOME/Library/Application Support/Herd/config/nvm"
 [[ -f "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh" ]] &&
   builtin source "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh"
 
-# Keep the repo's bin at the front after every chpwd so wrappers for npm,
-# yarn, npx and bun aren't shadowed by nvm's prepended bin.
-if [[ -d "${DOTFILES_DIR:-$HOME/dotfiles}/bin" ]]; then
-  _dotfiles-bin-to-front() {
-    local d="${DOTFILES_DIR:-$HOME/dotfiles}/bin"
-    # Prepend unless already first
-    [[ ${path[1]} = "$d" ]] || path=("$d" ${path:#$d})
-  }
-  autoload -U add-zsh-hook
-  add-zsh-hook chpwd _dotfiles-bin-to-front
-  _dotfiles-bin-to-front
-fi
-
 # Hard-coded to $HOME rather than a literal path so this works on any machine.
 export HERD_PHP_85_INI_SCAN_DIR="$HOME/Library/Application Support/Herd/config/php/85/"
 export HERD_PHP_84_INI_SCAN_DIR="$HOME/Library/Application Support/Herd/config/php/84/"
